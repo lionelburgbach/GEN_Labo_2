6239ed517fac0a86caf79bbbb9bed990b3d837d5
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-27 17:39:47 +0100

    Page 25 N.2 fixes #3

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -20,6 +20,7 @@ public class DollarTest {
     public void testEquality(){
 
         assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
 }
