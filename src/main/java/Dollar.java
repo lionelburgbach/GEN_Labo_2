1fd10216909d923f384e306af9ef9d4b111ba143
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-27 21:49:11 +0100

    forgot to delet method currency()

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -3,10 +3,4 @@ public class Dollar extends Money{
     Dollar(int amount, String currency) {
         super(amount, currency);
     }
-
-    String currency() {
-
-        return currency;
-    }
-
 }
