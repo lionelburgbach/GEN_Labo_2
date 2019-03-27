542b02bdc847ef65465194b710ac471d9d683a74
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-27 17:30:13 +0100

    Page 23 fixes 2

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -9,8 +9,11 @@ public class DollarTest {
     public void testMultiopication() {
 
         Dollar five = new Dollar(5);
+        Dollar product= five.times(2);
         five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 
 }
