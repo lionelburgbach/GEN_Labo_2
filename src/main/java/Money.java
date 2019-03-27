d67954954abbddb4a129398ebe5376d6b3ec929a
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-27 23:23:08 +0100

    Page 53 issue 13

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression{
 
     protected int amount;
     protected String currency;
@@ -38,7 +38,7 @@ class Money {
         return amount + " " + currency;
     }
 
-    Money plus(Money addend) {
+    Expression plus(Money addend) {
         return new Money(amount + addend.amount, currency);
     }
 }
