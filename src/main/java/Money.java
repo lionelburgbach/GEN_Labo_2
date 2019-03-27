665a4ec3e0a3202a223349b9841c8c2e734d2250
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-27 21:39:09 +0100

    Page 47 N.2  issue 9

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -11,7 +11,7 @@ class Money {
     public boolean equals(Object object) {
 
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency.equals(money.currency);
     }
 
     static Money dollar(int amount) {
@@ -26,7 +26,7 @@ class Money {
 
     Money times(int multiplier){
 
-        return null;
+        return new Money(amount * multiplier, currency);
     }
 
     String currency(){
