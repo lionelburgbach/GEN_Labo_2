3fe0cc025c1de32dc0d74fdbec20e59cc481cdd6
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-27 18:31:50 +0100

    Page 33 N.2 issue 6

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -12,7 +12,7 @@ public class Dollar extends Money{
 
     public boolean equals(Object object) {
 
-        Money dollar = (Dollar) object;
+        Money dollar = (Money) object;
         return amount == dollar.amount;
     }
 }
