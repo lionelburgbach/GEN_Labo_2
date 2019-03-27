9db8f241335586972c103b8c1e147c48dfdd0423
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-03-28 00:30:27 +0100

    Page 65 issue 16

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -70,4 +70,16 @@ public class MoneyTest {
         assertEquals(1, new Bank().rate("USD", "USD"));
     }
 
+    @Test
+    public void testMixedAdition() {
+
+        Money fiveBucks = Money.dollar(5);
+        Money tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result= bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+
+    }
+
 }
